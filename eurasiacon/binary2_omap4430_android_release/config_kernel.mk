b311abdd96219af8a91f1bc980cb613409ec38a4
Pankaj Bharadiya <pankaj.bharadiya@ti.com>
2012-02-15 15:25:23 +0530

    Removed stale binraries.
    Signed-off-by: Pankaj Bharadiya <pankaj.bharadiya@ti.com>

--- eurasiacon/binary2_omap4430_android_release/config_kernel.mk
+++
@@ -1,8 +0,0 @@
-override KERNELDIR := /datalocal/psp/pankaj/rowboat-ics/kernel
-override KERNEL_COMPONENTS := srvkm dc_omapfb3_linux
-override KERNEL_CROSS_COMPILE := arm-eabi-
-override PVR_SYSTEM := omap3
-override BUILD := release
-override DEBUGLINK := 1
-override LDM_PLATFORM := 1
-override OPTIM := -Os
